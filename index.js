e8268ddc7927cb5b22d03a3d19554a9211f4759d
clonalejandro <clonalejandromcb@gmail.com>
2020-08-16 17:48:04 +0200

    Improvements

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const { app, BrowserWindow } = require('electron')
 const { createWindowHook } = require('./hooks')
 
 app.whenReady().then(createWindowHook)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindowHook()
