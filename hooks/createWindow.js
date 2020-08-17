cc9036d3121faa02237c6e89988e34625070cd17
clonalejandro <clonalejandromcb@gmail.com>
2020-08-17 03:07:08 +0200

    Add icons

--- hooks/createWindow.js
+++ hooks/createWindow.js
@@ -1,4 +1,5 @@
-const { BrowserWindow } = require('electron')
+const path = require('path')
+const { BrowserWindow, nativeImage, app } = require('electron')
 const readConfigHook = require('./readConfig')
 
 const settings = {
@@ -11,6 +12,13 @@ const settings = {
 
 module.exports = () => {
   const window = new BrowserWindow(settings)
+  const icon = nativeImage.createFromPath(path.join(__dirname, '../icons/icon.png'))
+  
+  window.setTitle('Cloud')
+  
+  window.setIcon(icon)
+  app.dock.setIcon(icon)
+  
   readConfigHook((err, data) => {
     if (err) window.loadFile('./views/setup.html')
     else window.loadURL(JSON.parse(data).url)
